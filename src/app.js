cf271c64deab2c8ed10a38a89f2f7d329ffa1b6c
p021a <p021a@yahoo.co.uk>
2025-06-05 14:05:39 +0100

    PatchUserID: Fix user update validation logic and update related tests

--- src/app.js
+++ src/app.js
@@ -6,9 +6,11 @@ const {
   updateItemById,
   postItem,
   deleteItemById,
-
 } = require("./app/controller/item.controller.js");
-const { getUserById } = require("./app/controller/user.controller.js");
+const {
+  getUserById,
+  updateUserById,
+} = require("./app/controller/user.controller.js");
 const app = express();
 
 app.use(express.json());
@@ -17,25 +19,24 @@ app.get("/api/users/:userId", getUserById);
 app.get("/api/items", getItems);
 app.get("/api/items/:item_id", getItemById);
 
-
 app.patch("/api/items/:item_id", updateItemById);
+app.patch("/api/users/:userId", updateUserById);
 
 app.post("/api/items", postItem);
 
-app.delete("/api/items/:item_id", deleteItemById )
-
+app.delete("/api/items/:item_id", deleteItemById);
 
 app.all("/*splat", (req, res) => {
   res.status(404).send({ msg: "Path not found!" });
 });
 
-app.use((err, req, res, next) => {
-  if (err.name) {
-    return res.status(400).send({ msg: "Invalid user ID" });
-  } else {
-    next(err);
-  }
-});
+// app.use((err, req, res, next) => {
+//   if (err.name) {
+//     return res.status(400).send({ msg: "Invalid user ID" });
+//   } else {
+//     next(err);
+//   }
+// });
 
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
