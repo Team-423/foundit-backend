dbb2d56ebf1a38be51e684425820a66e4b26fbeb
kevpstephens <kevpstephenson@gmail.com>
2025-06-09 01:47:37 +0100

    Added cors to app.js for frontend integration

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const { getEndPoints } = require("./app/controller/app.controller.js");
 const {
   getItemById,
@@ -17,9 +18,12 @@ const { getColours } = require("./app/controller/colour.controller.js");
 const { getAllBrands } = require("./app/controller/brand.controller.js");
 const { getAllLocations } = require("./app/controller/location.controller.js");
 const { getCategories } = require("./app/controller/category.controller.js");
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.get("/api", getEndPoints);
 app.get("/api/users/:userId", getUserById);
 app.get("/api/items", getItems);
