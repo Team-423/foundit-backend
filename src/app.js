c7c39f9dd9d160b2c219c53624e7d919304f5ecb
carlyzhong <zhongyjcarly@gmail.com>
2025-06-11 02:34:12 +0100

    added patch questions endpoint for posting new item

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ const {
   getQandAForItem,
   postQandAForItem,
   patchAnswersForItem,
+  patchQuestionsForItem,
 } = require("./app/controller/qanda.controller.js");
 const { getCategories } = require("./app/controller/category.controller.js");
 
@@ -54,6 +55,7 @@ app.get("/api/users/:userId/items", getItemsByUserId);
 app.patch("/api/items/:item_id", updateItemById);
 app.patch("/api/items/:item_id/resolved", patchItemResolvedById);
 app.patch("/api/items/:item_id/QandA", patchAnswersForItem);
+app.patch("/api/items/:item_id/QandA/questions", patchQuestionsForItem);
 app.patch("/api/users/:userId", updateUserById);
 
 app.post("/api/items", postItem);
